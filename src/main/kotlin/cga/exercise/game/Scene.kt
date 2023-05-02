0f1007d58dab8f74770fb15aeb8fd6431fb6f21e
lhetflei <Loris3285!>
2023-05-02 19:32:03 +0200

    1.2.5 backface Culling+ Initals

--- src/main/kotlin/cga/exercise/game/Scene.kt
+++ src/main/kotlin/cga/exercise/game/Scene.kt
@@ -21,7 +21,7 @@ class Scene(private val window: GameWindow) {
 
     //scene setup
     init {
-        val vertices = floatArrayOf(
+        /*val vertices = floatArrayOf(
             -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
             0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
             0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
@@ -33,10 +33,50 @@ class Scene(private val window: GameWindow) {
             0, 1, 2,
             0, 2, 4,
             4, 2, 3
+        )*/
+        val vertices = floatArrayOf(
+                //L
+                -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+                -0.4f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+                -0.4f, 0.4f, 0.0f, 0.0f, 1.0f, 0.0f,
+                -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+                -0.4f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.1f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.1f, -0.4f, 0.0f, 1.0f, 0.0f, 0.0f,
+                -0.5f, -0.4f, 0.0f, 1.0f, 0.0f, 0.0f, //7
+                //H
+                0.4f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.5f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.4f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.7f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.8f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.7f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+                0.8f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,//15
+                0.5f, 0.4f, 0.0f, 0.0f, 0.0f, 1.0f,
+                0.7f, 0.4f, 0.0f, 0.0f, 0.0f, 1.0f,
+                0.5f, 0.3f, 0.0f, 0.0f, 1.0f, 0.0f,
+                0.7f, 0.3f, 0.0f, 1.0f, 0.0f, 0.0f,
+
         )
 
+        val indices = intArrayOf(
+                //L
+                0, 2, 1,
+                0, 3, 2,
+                3, 4, 2,
+                4, 5, 6,
+                7, 4, 6,
+                //H
+                8, 10, 9,
+                9, 10 ,11,
+                12,14,13,
+                13,14,15,
+                16,18,17,
+                17,18,19
+        )
         // todo
-
+        enableFaceCulling(GL_CW, GL_FRONT)
         //initial opengl state
         glClearColor(0.0f, 0.533f, 1.0f, 1.0f); GLError.checkThrow()
         val vertexAttribute =arrayOf<VertexAttribute>(VertexAttribute(3, GL_FLOAT,24,0),VertexAttribute(3, GL_FLOAT,24,12))
