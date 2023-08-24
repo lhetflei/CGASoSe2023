37ae87148de5d4bd0bd66dd50501bd8ebb66732b
P-Karsten <pascal.karsten@smail.th-koeln.de>
2023-08-24 19:59:16 +0200

    fixes, menu change

--- assets/shaders/tron_frag.glsl
+++ assets/shaders/tron_frag.glsl
@@ -35,11 +35,6 @@ uniform float shader;
 
 out vec4 color;
 
-// Gammakorrektur-Funktion
-/*float gamma(float value, float gammaValue) {
-    return pow(value, 1.0 / gammaValue);
-}*/
-
 vec3 gamma(vec3 C_linear) {
     float gammaValue = 2.2;  // Gamma value, adjust as needed
     return pow(C_linear, vec3(1.0 / gammaValue));
@@ -66,7 +61,6 @@ void main()
 if (shader>=0&&shader<=1){
     vec3 viewDir = normalize(vertexData.viewDir);
     vec3 normal = normalize(vertexData.normal);
-    //vec3 lightDirpoint = normalize(vertexData.lightDirpoint);
     vec3 lightDirspot = normalize(vertexData.lightDirspot);
 
     vec4 emissiveCol = texture(material_emissive, vertexData.tc)*vec4(emit_col,1.0);
@@ -76,7 +70,6 @@ if (shader>=0&&shader<=1){
     vec4 ambientCol = vec4(0.04, 0.04, 0.04, 1.0);
 
     // Gammakorrektur für diffuse, specular und emissive Farbwerte
-
     vec3 linearDiffuseCol = gamma(diffuseCol.xyz);
     vec3 linearSpecularCol = gamma(specularCol.xyz);
     vec3 linearEmissiveCol = gamma(emissiveCol.xyz);
@@ -93,12 +86,7 @@ if (shader>=0&&shader<=1){
         float distance = length(vertexData.lightDirpoint[i]);
         float attenuation = 100.0 / (distance * distance);  // attenuation
         color.xyz += brdf(normal, lightDirpoint, viewDir, linearSpecularCol, linearDiffuseCol, shininess, attenuation) * pointLight.lightColor[i];
-
-        /*vec3 lightDirpoint = normalize(vertexData.lightDirpoint[i]);
-        color.xyz += brdf(normal, lightDirpoint, viewDir, linearSpecularCol, linearDiffuseCol, shininess) * pointLight.lightColor[i];*/
     }
-
-    //color.xyz += brdf(normal, lightDirpoint, viewDir, linearSpecularCol, linearDiffuseCol, shininess) * pointLight.lightColor;
 
     vec3 lightDirection = normalize(spotLight.direction);
     float theta = dot(-lightDirspot, lightDirection);
@@ -109,7 +97,6 @@ if (shader>=0&&shader<=1){
     color.xyz += brdf(normal, lightDirspot, viewDir, linearSpecularCol, linearDiffuseCol, shininess,1.0/(length(vertexData.lightDirspot)*length(vertexData.lightDirspot))) * spotLight.lightColor * intensity;
 
     // Inverse Gammakorrektur, um das Ergebnis in sRGB oder Gamma zu konvertieren
-
     color.xyz = invgamma(color.xyz, gammaValue);
 
     color.a = 1.0;
@@ -121,7 +108,6 @@ if(shader>1&&shader<=2)
     vec3 linearDiffuseCol = gamma(diffuseCol.xyz);
     vec3 linearSpecularCol = gamma(specularCol.xyz);
     vec3 intensity;
-    //float intensity;
     vec4 ambientCol = vec4(0.04, 0.04, 0.04,1.0);
     vec4 emissiveCol = texture(material_emissive, vertexData.tc)*vec4(emit_col,1.0);
     intensity= ambientCol.xyz+emissiveCol.xyz;
@@ -145,24 +131,6 @@ if(shader>1&&shader<=2)
     color = vec4(0.4,0.2,0.2,1.0);
     else
     color = vec4(0.1,0.05,0.05,1.0);
-
-    /*intensity += clamp((theta - gamma) / (phi - gamma), 0.0, 1.0)*100.0/(length(vertexData.lightDirspot)*length(vertexData.lightDirspot));
-    if (intensity > 0.65)
-    color = vec4(1.0, 0.5, 0.5, 1.0);
-    else if (intensity > 0.35)
-    color = vec4(0.6, 0.3, 0.3, 1.0);
-    else if (intensity > 0.2)
-    color = vec4(0.4, 0.2, 0.2, 1.0);
-    else
-    color = vec4(0.1, 0.1, 0.1, 1.0);*/
-
-
-
-
-
-
-
-
 }
     if(shader>2&&shader<=3)
     {
@@ -173,116 +141,3 @@ if(shader>1&&shader<=2)
         color = vec4(vertexData.color.x, vertexData.color.y, vertexData.color.z, 1.0);
     }
 }
-
-
-
-    //color = ambientTerm+ diffuseTerm*intensity+ diffuseTerm + specularTerm+specularTerm*intensity;
-
-
-/*#version 330 core
-
-// Input from vertex shader
-in struct VertexData
-{
-    vec3 color;
-    vec2 tc;
-    vec3 lightDir;
-    vec3 viewDir;
-    vec3 normal;
-} vertexData;
-
-uniform struct PointLight {
-    vec3 position;
-} pointLight;
-
-uniform struct SpotLight {
-    float innerConeAngle;
-    float outerConeAngle;
-    vec3 direction;
-    vec3 position;
-} spotLight;
-
-uniform sampler2D material_emissive;
-uniform sampler2D material_diffuse;
-uniform sampler2D material_specular;
-uniform float shininess;
-uniform vec3 lightColor;
-
-// Fragment shader output
-out vec4 color;
-
-void main()
-{
-    vec3 normal = normalize(vertexData.normal);
-    vec3 lightDir = normalize(vertexData.lightDir);
-
-
-
-    vec3 viewDir = normalize(vertexData.viewDir);
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float specular = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
-
-    vec4 emissiveCol = texture(material_emissive, vertexData.tc);
-    vec4 diffuseCol = texture(material_diffuse, vertexData.tc);
-    vec4 specularCol = texture(material_specular, vertexData.tc);
-
-    vec4 ambientCol = vec4(1.0, 1.0, 1.0, 1.0); // Ambient color
-
-    vec4 lightIntensity = vec4(lightColor, 1.0); // Light intensity
-
-    vec4 ambientTerm = emissiveCol * ambientCol;
-    vec4 diffuseTerm = diffuseCol * lightIntensity * max(dot(normal, lightDir), 0.0);
-    vec4 specularTerm = specularCol * lightIntensity  * pow(max(dot(viewDir, reflectDir), 0.0), shininess);
-
-    color = ambientTerm + diffuseTerm + specularTerm;
-}
-
-/*#version 330 core
-
-//input from vertex shader
-in struct VertexData
-{
-    vec3 color;
-    vec2 tc;
-    vec3 lightDir;
-    vec3 viewDir;
-    vec3 normal;
-} vertexData;
-
-uniform struct SpotLight {
-
-    float innerConeAngle;
-    float outerConeAngle;
-    vec3 direction;
-
-} spotLight;
-
-uniform sampler2D material_emissive;
-uniform sampler2D material_diffuse;
-uniform sampler2D material_specular;
-float shininess;
-uniform vec3 lightColor;
-//fragment shader output
-out vec4 color;
-
-void main(){
-
-
-
-    vec3 normal = normalize(vertexData.normal);
-    vec3 lightDir = normalize(vertexData.lightDir);
-    float diffuse = max(dot(normal,lightDir),0.0);
-
-    vec3 viewDir = normalize(vertexData.viewDir);
-    vec3 reflectDir = normalize(reflect(-lightDir,normal));
-    float scpecular = pow(max(dot(viewDir,reflectDir),0.0),shininess);
-
-    vec4 emissiveCol = texture(material_emissive,vertexData.tc);
-    vec4 diffuseCol = texture(material_diffuse,vertexData.tc);
-    vec4 specularCol = texture(material_specular,vertexData.tc);
-
-    color = emissiveCol * (diffuse*diffuseCol+scpecular*specularCol)* vec4(lightColor,1.0);
-
-    //color = vec4(emissiveColor.rgb, 1.0);
-}*/
-
